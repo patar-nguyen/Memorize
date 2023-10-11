c47104d4c86ac7464543885078f7c2fa2657f3d7
Patrick Nguyen <patar.nguyen@gmail.com>
2023-10-10 21:06:32 -0400

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Patrick Nguyen on 2023-10-10.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
